6231c0dc324f35956eae2c4d2c4c5f6e0ff20dde
Jim Hodapp <james.hodapp@gmail.com>
2020-07-24 17:26:06 -0500

    Initialize Oat++ library.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,30 @@
 // main.cpp
 // websocket client POC
 
-#include <iostream>
-
 #include "websocket_client_poc.h"
+
+#include "oatpp/network/client/SimpleTCPConnectionProvider.hpp"
+
+#include <iostream>
+#include <thread>
+
+namespace {
+
+    const char* TAG = "websocket-client-poc";
+
+}
+
+void run() {
+    OATPP_LOGI(TAG, "Application Started");
+}
 
 int main()
 {
     std::cout << "WebsocketClientPoc" << std::endl;
+
+    oatpp::base::Environment::init();
+    run();
+    oatpp::base::Environment::destroy();
+
     return 0;
 }
